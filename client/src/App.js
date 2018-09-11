605999cc1bc8cf17911fbf714211b9e82ecae0eb
Leo Cheung <lappangcheung@gmail.com>
2018-09-10 22:32:22 -0400

    fixing some issues

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+// Portfolio Nav & Footer
+import Navbar from './portfolio/components/pages/Navbar'
+import Footer from './portfolio/components/layouts/Footer'
+
+// Portfolio Components
+import Home from './portfolio/components/pages/Home'
+import About from './portfolio/components/pages/About'
+import Contact from './portfolio/components/pages/Contact'
+import Work from './portfolio/components/pages/Work'
+
+// Dashboard Components
+import Login from './dashboard/Login'
+import Register from './dashboard/Register'
+
+import './portfolio/styles/style.css'
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <Fragment>
+                    <Navbar />
+                    <Route exact path='/' component={ Home } />
+                    <Route exact path='/about' component={ About } />
+                    <Route exact path='/contact' component={ Contact } />
+                    <Route exact path='/work' component={ Work } />
+                    <Route exact path='/login' component={ Login } />
+                    <Route exact path='/register' component={ Register } />
+                    <Footer />
+                </Fragment>
+            </Router>
+        )
+    }
+}
+
+export default App
