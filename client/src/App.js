57837864f1650b2b22828d44eb4804f464ca603b
Leo Cheung <lappangcheung@gmail.com>
2018-09-11 14:46:19 -0400

    check if use is authenticated

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import React, { Component, Fragment } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import jwt_decode from 'jwt-decode'
+
 import { Provider } from 'react-redux'
 
 import store from './store'
+import setAuthToken from './util/setAuthToken'
+import {setCurrentUser} from './actions/authActions'
 
 
 // Portfolio Nav & Footer
@@ -23,8 +27,18 @@ import Profile from './dashboard/components/pages/Profile'
 
 // CSS for everything
 import './portfolio/styles/main.css'
+import { decode } from 'punycode';
 
+// Check for token
+if(localStorage.jwtToken){
+    // Set auth token header auth
+    setAuthToken(localStorage.jwtToken)
+    // Decoded token and get user info and expiration
+    const decoded = jwt_decode(localStorage.jwtToken)
+    // Set user and isAuthenticated
+    store.dispatch(setCurrentUser(decoded))
 
+}
 
 class App extends Component {
     render() {
