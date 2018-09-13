981930c135c50d6928fcc8f95fa43cb8f00f2986
Leo Cheung <lappangcheung@gmail.com>
2018-09-12 20:46:24 -0400

    modifying the dashboard

--- client/src/dashboard/components/pages/Dashboard.js
+++ client/src/dashboard/components/pages/Dashboard.js
@@ -5,13 +5,46 @@ import { connect } from 'react-redux'
 import { getCurrentProfile } from '../../../redux/actions/profileActions'
 
 class Dashboard extends Component{
+
+
+    componentDidMount(){
+        this.props.getCurrentProfile()
+    }
+
     render(){
+
+        const { user } = this.props.auth
+        const { profile, loading } = this.props.profile
+
+        let dashboardContent
+
+        if(profile === null || loading){
+            dashboardContent = <h1 className="lg-heading">
+                Load<span className="text-secondary"><strong>ing...</strong></span>
+                </h1>
+        }else{
+            dashboardContent = <h1 className="lg-heading">
+                Dash<span className="text-secondary"><strong>board</strong></span>
+            </h1>
+        }
+
         return(
-            <div>
-                Dashboard
-            </div>
+            <main id="dashboard">
+                {dashboardContent}
+            </main>
         )
     }
 }
 
-export default Dashboard
+Dashboard.propTypes = {
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state =>( {
+    profile: state.profile,
+    auth: state.auth,
+})
+
+export default connect(mapStateToProps,{getCurrentProfile})(Dashboard)
